bfc4d758ca365810cda7b8a6b14740c6349beb6d
Dariel Kremov <dkremov@gmail.com>
2014-12-05 20:08:19 +0200

    the real stuff

---
+++ jni/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include jni/pixman.mk
+include jni/cairo.mk
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := libcairo-android
+LOCAL_CFLAGS    := -O2 --std=c99 -I. -Ijni/pixman/pixman -Ijni/cairo/src -Ijni/cairo-extra -Ijni/pixman-extra -Wno-missing-field-initializers
+LOCAL_SRC_FILES := pure-ndk.c
+LOCAL_STATIC_LIBRARIES := libcairo libpixman cpufeatures
+
+include $(BUILD_SHARED_LIBRARY)
+$(call import-module,android/cpufeatures)
---
+++ jni/pure-ndk.c
@@ -0,0 +1,10 @@
+#include <jni.h>
+
+jint
+Java_com_example_Nothing_add( JNIEnv*  env,
+                              jobject  this,
+                              jint     x,
+                              jint     y )
+{
+    return 0;
+}
